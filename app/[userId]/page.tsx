6a3af8e09fdae31299cfa4ef3c6b0164513c933d
Jonathan Viray <jfviray7@gmail.com>
2024-05-23 16:13:10 -0500

    Add markup for user history on UserPage

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -2,14 +2,15 @@ import { cn } from '@/lib/utils';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const TABS = ['Comments', 'Posts'];
 
 export default function UserPage() {
   return (
-    <>
+    <div className="w-[800px]">
       {/* Banner */}
-      <div className="w-[700px]">
+      <div>
         <header className="flex items-center gap-8 rounded-t-[3px] bg-border p-11">
           <Avatar className="h-28 w-28 border-8 border-background">
             <AvatarImage src="https://github.com/shadcn.png" />
@@ -18,7 +19,7 @@ export default function UserPage() {
 
           <div>
             <h1 className="text-[36px] font-semibold leading-relaxed text-white">
-              Content Marketing
+              Jonathan Viray
             </h1>
             <p className="text-lg leading-snug">
               Lorem ipsum dolor sit amet consectetur adipisicing elit
@@ -26,7 +27,7 @@ export default function UserPage() {
           </div>
         </header>
 
-        <nav className="flex items-center justify-between rounded-b-[3px] border-t-[3px] border-background bg-[#182e43] pr-2">
+        <nav className="flex items-center justify-between rounded-b-[3px] border-t-[3px] border-background bg-[#182e43]">
           {/* Tabs */}
           <ul className="flex gap-2">
             {TABS.map((tabLabel, idx) => (
@@ -44,19 +45,52 @@ export default function UserPage() {
             ))}
           </ul>
 
-          <Button
-            size={'lg'}
-            className="bg-gradient-to-r from-[#e052a0] to-[#f15c41] text-base font-semibold  hover:from-[#3ec7e0] hover:to-[#526bf4]"
-          >
-            Follow
-          </Button>
+          <div className="px-2">
+            <Button
+              size={'lg'}
+              className="bg-gradient-to-r from-[#e052a0] to-[#f15c41] text-base font-semibold  hover:from-[#3ec7e0] hover:to-[#526bf4]"
+            >
+              Follow
+            </Button>
+          </div>
         </nav>
       </div>
 
-      {/* <ul>
-        <li>Post 1</li>
-        <li>Post 2</li>
-      </ul> */}
-    </>
+      {/* Feed */}
+      <div className="py-10">
+        <ul className="space-y-12">
+          <li className="space-y-4">
+            <div className="space-x-2 text-[15px]">
+              <span className="font-semibold text-ring">MAY 22 2024</span>
+              <Link href={'/'} className="hover:underline">
+                replied to a post
+              </Link>
+            </div>
+
+            <div className="rounded-[3px] bg-border p-4 text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+              voluptas amet officia earum nesciunt repudiandae nisi magni dicta
+              deserunt ipsam!
+            </div>
+          </li>
+
+          <li className="space-y-4">
+            <div className="space-x-2 text-[15px]">
+              <span className="font-semibold text-ring">MAY 22 2024</span>
+              <Link href={'/'} className="hover:underline">
+                replied to a post
+              </Link>
+            </div>
+
+            <div className="rounded-[3px] bg-border p-4 text-lg">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus, laborum voluptatum! Debitis, vitae vero! Soluta
+              inventore ea praesentium culpa sunt reprehenderit corporis debitis
+              saepe consequatur. Reprehenderit animi libero aperiam at.
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
