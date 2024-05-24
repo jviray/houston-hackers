4729b16c0fd94ecc9cf41f12b66865fe96c7f2f5
Jonathan Viray <jfviray7@gmail.com>
2024-05-24 16:03:30 -0500

    Edit Dialog for sign up button

--- components/layout/auth-btn-group.tsx
+++ components/layout/auth-btn-group.tsx
@@ -1,10 +1,10 @@
+import { FaGoogle } from 'react-icons/fa';
+
 import { Btn } from '@/components/btn';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
-  DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog';
 
@@ -19,14 +19,21 @@ export const AuthButtonGroup = () => {
         <DialogTrigger asChild>
           <Btn className="font-semibold">SIGN UP</Btn>
         </DialogTrigger>
-        <DialogContent>
+
+        <DialogContent className="w-[448px]">
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
+            <h2 className="text-xl font-bold text-white">
+              Welcome to Houston Hackers!
+            </h2>
           </DialogHeader>
+
+          <Btn
+            size="lg"
+            className="flex items-center justify-center gap-4 text-base font-semibold"
+          >
+            <FaGoogle className="h-5 w-5" />
+            Continue with Google
+          </Btn>
         </DialogContent>
       </Dialog>
     </>
