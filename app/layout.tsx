1e66756372327a9ca912128f1f6f35f99c39434e
Jonathan Viray <jfviray7@gmail.com>
2024-05-24 01:25:40 -0500

    Add base markup for layout Header; edit main content container

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Source_Sans_3 } from 'next/font/google';
 
 import '@/styles/globals.css';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 const sourceSans3 = Source_Sans_3({ subsets: ['latin'] });
 
@@ -18,7 +19,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={sourceSans3.className}>
-        <main className="h-full p-6">{children}</main>
+        <header className="bg-border px-6">
+          <div className="mx-auto flex max-w-screen-lg items-center justify-between py-3">
+            <h1 className="font-bold text-white">HOUSTON HACKERS</h1>
+            <Avatar className="outline-3 outline-white hover:outline">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        </header>
+
+        <main className="h-full p-8">
+          <div className="mx-auto grid max-w-screen-lg place-items-center">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
