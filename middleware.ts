3a53906a627b8f0a78ead5227580514b3f6db07e
Jonathan Viray <jfviray7@gmail.com>
2024-05-27 14:51:29 -0500

    Add initial setup for middleware file

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { type NextRequestWithAuth, withAuth } from 'next-auth/middleware';
+
+// `withAuth` augments a `Request` with user's token, then passes it middleware callback
+export default withAuth(function middleware(req: NextRequestWithAuth) {});
+
+// Regex below will cause middleware to be invoked on all routes except some
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
