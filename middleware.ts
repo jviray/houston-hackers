94f7e268f39b0091ad39931d4866e8dfa9a668bb
Jonathan Viray <jfviray7@gmail.com>
2024-05-27 16:07:03 -0500

    Edit middleware file to allow all

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
 import { type NextRequestWithAuth, withAuth } from 'next-auth/middleware';
 
 // `withAuth` augments a `Request` with user's token, then passes it middleware callback
-export default withAuth(function middleware(req: NextRequestWithAuth) {});
+export default withAuth(
+  function middleware(req: NextRequestWithAuth) {
+    return null;
+  },
+  {
+    callbacks: {
+      // Return true to always execute middleware
+      authorized: () => true,
+    },
+  },
+);
 
 // Regex below will cause middleware to be invoked on all routes except some
 export const config = {
