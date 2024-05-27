10eae8f9ba86e8beb0588c61d5d50a319a526ca8
Jonathan Viray <jfviray7@gmail.com>
2024-05-27 13:42:33 -0500

    Configure GoogleOAuth provider for next-auth

--- config/auth.ts
+++ config/auth.ts
@@ -1,3 +1,11 @@
 import { type NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-export const options = { providers: [] } satisfies NextAuthOptions;
+export const options = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+} satisfies NextAuthOptions;
