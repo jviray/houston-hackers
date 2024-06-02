87ea8f68ed226e713f45bbcac672102255bacb69
Jonathan Viray <jfviray7@gmail.com>
2024-06-02 00:57:32 -0500

    Edit editor css, add HistoryPlugin

--- components/posts/editor.tsx
+++ components/posts/editor.tsx
@@ -4,6 +4,7 @@ import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
 import { ContentEditable } from '@lexical/react/LexicalContentEditable';
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 
 const theme = {};
@@ -22,16 +23,21 @@ export default function Editor() {
   return (
     <div className="w-8/12">
       <LexicalComposer initialConfig={initialConfig}>
-        <div className="min-h-44 w-full rounded-md border-2 bg-border p-4 text-xl text-[#dde1e4]">
-          <RichTextPlugin
-            contentEditable={
-              <ContentEditable autoFocus className="focus:outline-none" />
-            }
-            placeholder={<p className="text-[#63809C]">Enter body...</p>}
-            ErrorBoundary={LexicalErrorBoundary}
-          />
-          <AutoFocusPlugin />
+        <div className="flex min-h-44 w-full flex-col rounded-md border-2 bg-border p-4 text-xl text-[#dde1e4]">
+          <div className="relative flex flex-grow flex-col">
+            <RichTextPlugin
+              contentEditable={
+                <ContentEditable className="relative z-10 h-full w-full flex-grow focus:outline-none" />
+              }
+              placeholder={
+                <p className="absolute top-0 text-[#63809C]">Enter body...</p>
+              }
+              ErrorBoundary={LexicalErrorBoundary}
+            />
+          </div>
         </div>
+        <AutoFocusPlugin />
+        <HistoryPlugin />
       </LexicalComposer>
     </div>
   );
