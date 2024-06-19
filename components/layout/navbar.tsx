2fee1ecc79d5ac7248faaa724af2e578391f839d
Jonathan Viray <jfviray7@gmail.com>
2024-06-19 16:10:49 -0500

    Add link to homepage from Navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,6 +2,7 @@ import { getCurrentUser } from '@/server/queries/users';
 
 import { AuthButtonGroup } from '@/components/layout/auth-btn-group';
 import { Avatar } from '@/components/user/avatar';
+import Link from 'next/link';
 
 export const Navbar = async () => {
   const user = await getCurrentUser();
@@ -10,7 +11,9 @@ export const Navbar = async () => {
   return (
     <header className="bg-border px-6">
       <div className="mx-auto flex max-w-screen-lg items-center justify-between py-3">
-        <h1 className="font-bold text-white">HOUSTON HACKERS</h1>
+        <h1 className="font-bold text-white">
+          <Link href="/">HOUSTON HACKERS</Link>
+        </h1>
 
         <div className="space-x-3">
           {isLoggedIn ? (
