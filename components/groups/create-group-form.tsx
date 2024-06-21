6fc497c21c8faa9eef01345abd3e6c76335bffd3
Jonathan Viray <jfviray7@gmail.com>
2024-06-21 16:19:33 -0500

    Add form fields for group name and description

--- components/groups/create-group-form.tsx
+++ components/groups/create-group-form.tsx
@@ -1,11 +1,13 @@
 'use client';
 
+import { ChangeEvent, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { ImageUp } from 'lucide-react';
 
 import { GroupSchema } from '@/lib/schemas';
+import { getSignedUrl } from '@/server/actions/upload';
 
 import {
   Dialog,
@@ -16,8 +18,16 @@ import {
 import { Btn } from '@/components/btn';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Input } from '@/components/ui/input';
-import { ChangeEvent, useState } from 'react';
-import { getSignedUrl } from '@/server/actions/upload';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+// import { AutosizeTextarea } from '@/components/ui/autosize-textarea';
+import { Textarea } from '@/components/ui/textarea';
 
 /**
  * TO DO:
@@ -75,30 +85,84 @@ export const CreateGroupForm = () => {
         </DialogHeader>
 
         {/* Form */}
-        <form onSubmit={form.handleSubmit(onSubmit)} className="mt-3">
-          <Avatar
-            onClick={() => document.getElementById('image-input')!.click()}
-            className="group relative grid h-24 w-24 cursor-pointer place-items-center border-[6px] text-center text-xs"
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-3 space-y-8"
           >
-            <AvatarImage src={fileUrl} />
-            <AvatarFallback className="bg-[#182e43]">Add photo</AvatarFallback>
+            <div className="space-y-6">
+              <div className="flex space-x-6">
+                <Avatar
+                  onClick={() =>
+                    document.getElementById('image-input')!.click()
+                  }
+                  className="group relative grid h-24 w-24 cursor-pointer place-items-center border-[6px] text-center text-xs"
+                >
+                  <AvatarImage src={fileUrl} />
+                  <AvatarFallback className="bg-[#182e43]">
+                    Add image
+                  </AvatarFallback>
 
-            {/* Overlay */}
-            <div className="absolute bottom-0 left-0 right-0 top-0 group-hover:bg-black group-hover:opacity-40"></div>
-            {/* Icon Overlay */}
-            <div className="absolute hidden h-full w-full group-hover:grid group-hover:place-items-center">
-              <ImageUp color="#fff" size={28} />
+                  {/* Overlay */}
+                  <div className="absolute bottom-0 left-0 right-0 top-0 group-hover:bg-black group-hover:opacity-40"></div>
+                  {/* Icon Overlay */}
+                  <div className="absolute hidden h-full w-full group-hover:grid group-hover:place-items-center">
+                    <ImageUp color="#fff" size={28} />
+                  </div>
+
+                  <Input
+                    id="image-input"
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    onChange={onImageChange}
+                  />
+                </Avatar>
+
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Group Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="e.g. E-Commerce, Bootstrapping, etc."
+                          className="appearance-none rounded-[3px] border-none bg-border text-base text-[#dde1e4]"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        maxLength={280}
+                        placeholder="What's this group about?"
+                        className="h-40 w-full resize-none appearance-none rounded-[3px] border-none bg-border text-base text-[#dde1e4]"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
 
-            <Input
-              id="image-input"
-              type="file"
-              accept="image/*"
-              className="hidden"
-              onChange={onImageChange}
-            />
-          </Avatar>
-        </form>
+            <div className="flex justify-end">
+              <Btn type="submit">SUBMIT GROUP</Btn>
+            </div>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
--- lib/schemas.ts
+++ lib/schemas.ts
@@ -20,12 +20,15 @@ export const GroupSchema = z.object({
   name: z
     .string({ required_error: 'Name is required' })
     .min(3, 'Name must be at least 3 characters.')
-    .max(21, 'Name cannot be longer than 21 characters.'),
+    .max(21, 'Name cannot be longer than 21 characters.')
+    .refine((val) => val !== '', {
+      message: 'Name is required.',
+    }),
   image: z.string().url().optional(),
   description: z
     .string({ required_error: 'Description is required' })
     .min(6, 'Description must be at least 6 characters.')
-    .max(140, 'Description cannot be longer than 140 characters.')
+    .max(280, 'Description cannot be longer than 280 characters.')
     .refine((val) => val !== '', {
       message: 'Description is required.',
     }),
