63795727d8bc66bdbd945ac9b8137de959aa7d06
Jonathan Viray <jfviray7@gmail.com>
2024-07-02 12:58:29 -0500

    Add displayName for custom avatar component

--- components/user/avatar.tsx
+++ components/user/avatar.tsx
@@ -25,36 +25,38 @@ const BoringAvatarVariants = {
   group: 'bauhaus',
 } as const; // Explained: https://stackoverflow.com/questions/72340281/ts2322-type-string-is-not-assignable-to-type-union-of-strings
 
-const Avatar =
-  <T extends AvatarAccount>(type: 'user' | 'group') =>
-  (props: AvatarProps<T>) => {
-    const { className, asLink = false, data } = props;
+const Avatar = <T extends AvatarAccount>(type: 'user' | 'group') =>
+  Object.assign(
+    (props: AvatarProps<T>) => {
+      const { className, asLink = false, data } = props;
 
-    const component = (
-      <ShadcnAvatar className={className}>
-        <AvatarImage src={data.image as string | undefined} />
+      const component = (
+        <ShadcnAvatar className={className}>
+          <AvatarImage src={data.image as string | undefined} />
 
-        <Skeleton className="h-full w-full rounded-full" />
+          <Skeleton className="h-full w-full rounded-full" />
 
-        <AvatarFallback delayMs={1000}>
-          <BoringAvatar
-            size={100}
-            name={data.id}
-            variant={BoringAvatarVariants[type]}
-            colors={['#fb7968', '#f9c593', '#fafad4', '#b0d1b2', '#89b2a2']}
-          />
-        </AvatarFallback>
-      </ShadcnAvatar>
-    );
+          <AvatarFallback delayMs={1000}>
+            <BoringAvatar
+              size={100}
+              name={data.id}
+              variant={BoringAvatarVariants[type]}
+              colors={['#fb7968', '#f9c593', '#fafad4', '#b0d1b2', '#89b2a2']}
+            />
+          </AvatarFallback>
+        </ShadcnAvatar>
+      );
 
-    return asLink ? (
-      <Link href={`/${type === 'user' ? data.username : data.name}`}>
-        {component}
-      </Link>
-    ) : (
-      component
-    );
-  };
+      return asLink ? (
+        <Link href={`/${type === 'user' ? data.username : data.name}`}>
+          {component}
+        </Link>
+      ) : (
+        component
+      );
+    },
+    { displayName: 'Avatar' },
+  );
 
 export const UserAvatar = Avatar<User>('user');
 export const GroupAvatar = Avatar<Group>('group');
